9e8d7717387cfddb74e52b4befe5651013f65b67
Shehu-Fatiudeen Lawal <shehufatiudeen@gmail.com>
2023-09-16 01:32:46 +0100

    update

--- packages/utils/src/main/lazySingleton.ts
+++ packages/utils/src/main/lazySingleton.ts
@@ -1,13 +1,15 @@
-export function lazySingleton<T>(obj: new () => T) {
-  const instance = <T>(obj: { _instance: null | T } & (new () => T)): T => {
-    if (obj._instance) {
-      return obj._instance;
+export function lazySingleton<T>(obj: new (...args: any[]) => T) {
+  // obj:
+  const instance = (...args: any[]): T => {
+    const _obj = obj as unknown as { _instance: null | T } & (new () => T);
+    if (_obj._instance) {
+      return _obj._instance;
     }
-    obj._instance = new obj();
-    return obj._instance;
+    _obj._instance = new obj(...args);
+    return _obj._instance;
   };
   if (!('_instance' in obj)) {
     Object.assign(obj, { _instance: null, instance });
   }
-  return (<{ _instance: null | T; instance: typeof instance } & (new () => T)>obj).instance;
+  return (<{ _instance: null | T; instance: typeof instance } & (new (...args: any[]) => T)>obj).instance;
 }
